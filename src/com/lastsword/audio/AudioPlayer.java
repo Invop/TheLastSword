6e181dcf178452c113618e0080c80b8ee1c10c43
Invop <shandrykjj512@gmail.com>
2023-05-21 17:36:55 +0300

    AudioPlayer update

--- src/com/lastsword/audio/AudioPlayer.java
+++ src/com/lastsword/audio/AudioPlayer.java
@@ -5,6 +5,8 @@ import java.io.File;
 import java.io.IOException;
 
 public class AudioPlayer {
+    private Clip clip;
+    private boolean isPlaying;
 
     public AudioPlayer(String path) {
         play(path);
@@ -20,28 +22,49 @@ public class AudioPlayer {
 
             DataLine.Info info = new DataLine.Info(Clip.class, format);
 
-            Clip clip = (Clip) AudioSystem.getLine(info);
+            clip = (Clip) AudioSystem.getLine(info);
             clip.open(audioStream);
-
 
             FloatControl gainControl = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
 
-
-            float initialVolume = 0.2f; // 50%
+            float initialVolume = 0.3f; // 20%
             setVolume(gainControl, initialVolume);
 
+            clip.addLineListener(event -> {
+                if (event.getType() == LineEvent.Type.STOP) {
+                    if (isPlaying) {
+                        clip.setMicrosecondPosition(0);
+                        clip.start();
+                    }
+                }
+            });
+
+            isPlaying = true;
             clip.start();
 
-            // Sleep while the audio is playing
-            Thread.sleep(clip.getMicrosecondLength() / 1000);
-            clip.stop();
-            clip.close();
-            audioStream.close();
-
-        } catch (UnsupportedAudioFileException | LineUnavailableException | IOException | InterruptedException e) {
+        } catch (UnsupportedAudioFileException | LineUnavailableException | IOException e) {
             e.printStackTrace();
         }
     }
+
+    public void loop() {
+        if (clip != null) {
+            if (!isPlaying) {
+                isPlaying = true;
+                clip.setMicrosecondPosition(0);
+                clip.start();
+            }
+        }
+    }
+
+    public void stop() {
+        if (clip != null) {
+            isPlaying = false;
+            clip.stop();
+            clip.close();
+        }
+    }
+
     private void setVolume(FloatControl gainControl, float volume) {
         float dB = (float) (Math.log(volume) / Math.log(10.0) * 20.0);
         gainControl.setValue(dB);
--- src/com/lastsword/game/Game.java
+++ src/com/lastsword/game/Game.java
@@ -1,16 +1,25 @@
 package com.lastsword.game;
 
 import com.lastsword.audio.AudioPlayer;
+import com.lastsword.graphics.Animation;
+
+import java.awt.image.BufferedImage;
 
 public class Game {
     private GameWindow gameWindow;
     private GamePanel gamePanel;
     private AudioPlayer audioPlayer;
+    private Animation animation;
+    private boolean isRunning;
+    private int frameRate;
+
 
     public Game(){
         gamePanel = new GamePanel();
         gameWindow = new GameWindow(gamePanel);
         gamePanel.requestFocus();
-        audioPlayer = new AudioPlayer("src/res/music/Battle_3_Choir_Loop.wav");
+        audioPlayer = new AudioPlayer("src/res/music/MainTheme.wav");
+        audioPlayer.loop();
     }
+
 }
