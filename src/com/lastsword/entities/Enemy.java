32b4724a159f85c83e26695dac3176e39919f7cd
Invop <shandrykjj512@gmail.com>
2023-05-25 17:21:22 +0300

    Player and enemy fields

--- src/com/lastsword/entities/Enemy.java
+++ src/com/lastsword/entities/Enemy.java
@@ -1,4 +1,13 @@
 package com.lastsword.entities;
 
 public class Enemy {
+    private int enemyHp;
+
+    public int getEnemyHp() {
+        return enemyHp;
+    }
+
+    public void setEnemyHp(int enemyHp) {
+        this.enemyHp = enemyHp;
+    }
 }
--- src/com/lastsword/entities/Player.java
+++ src/com/lastsword/entities/Player.java
@@ -1,4 +1,28 @@
 package com.lastsword.entities;
 
 public class Player {
+    private int score;
+    private int playerHp;
+
+    private static final int ultChance  = 15;
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public int getPlayerHp() {
+        return playerHp;
+    }
+
+    public void setPlayerHp(int playerHp) {
+        this.playerHp = playerHp;
+    }
+
+    public int getUltChance(){
+        return ultChance;
+    }
 }
