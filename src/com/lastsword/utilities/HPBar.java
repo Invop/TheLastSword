33f5d6f2ea00a8503e4b6aeef6d6259f81624d79
Invop <shandrykjj512@gmail.com>
2023-05-25 15:17:36 +0300

    HpBar logic

---
+++ src/com/lastsword/utilities/HPBar.java
@@ -0,0 +1,38 @@
+package com.lastsword.utilities;
+
+public class HPBar {
+    private int maxHP;
+    private int currentHP;
+
+    public HPBar(int maxHP) {
+        this.maxHP = maxHP;
+        this.currentHP = maxHP;
+    }
+
+    public int getCurrentHP() {
+        return currentHP;
+    }
+
+    public void setCurrentHP(int currentHP) {
+        this.currentHP = currentHP;
+    }
+
+    public void decreaseHP(int amount) {
+        currentHP -= amount;
+        if (currentHP < 0) {
+            currentHP = 0;
+        }
+    }
+
+    public void increaseHP(int amount) {
+        currentHP += amount;
+        if (currentHP > maxHP) {
+            currentHP = maxHP;
+        }
+    }
+
+    public double getPercentage() {
+        return (double) currentHP / maxHP * 100;
+    }
+
+}
